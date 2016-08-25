da9444f7eec12725b8ee68d2a754ea9cb81dcc55
kai1004 <khoinguyen@Khois-iMac.local>
2016-08-25 01:35:24 -0400

    App beta

---
+++ AppBeta/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  devslopes-socials
+//
+//  Created by Khoi Nguyen on 8/24/16.
+//  Copyright © 2016 Khoi Nguyen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let Shadow_Gray: CGFloat = 120.0 / 255.0
+
